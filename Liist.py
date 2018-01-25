6fdce6de32091753c8ed34f3c00c7818b936a928
pythonmuli <thakurvicky.thakur505@gmail.com>
2018-01-25 04:15:07 -0500

    new

---
+++ Liist.py
@@ -0,0 +1,20 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> list=[1,2,3,4,5,6,7,8,9]
+>>> list[:8],list[1:-],list[1:2:1],list[-1]
+SyntaxError: invalid syntax
+>>> 
+>>> list=[1,2,3,4,5,6,7,8,9]
+>>> 
+KeyboardInterrupt
+>>>  list=[1,2,3,4,5,6,7,8,9]
+SyntaxError: unexpected indent
+>>> 
+>>> List=[1,2,3,4,5,6,7,8,9]
+>>> List[:8],List[1: ],List[1:2:1],List[-1]
+([1, 2, 3, 4, 5, 6, 7, 8], [2, 3, 4, 5, 6, 7, 8, 9], [2], 9)
+>>> list[1:6:1]
+[2, 3, 4, 5, 6]
+>>> list[1:5:2]
+[2, 4]
+>>> 
